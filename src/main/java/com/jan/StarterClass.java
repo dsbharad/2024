7cac104b3aa0a3054a05708eb04eb8e33ee291f1
bharadwaj <bharadwaj.ds@gmail.com>
2024-01-14 13:02:14 +0530

    LoggerIsUsed.RomanNumberToIntegerIsCompleted.

--- src/main/java/com/jan/StarterClass.java
+++ src/main/java/com/jan/StarterClass.java
@@ -1,40 +1,59 @@
 package com.jan;
 
-import com.jan.commons.annotations.PickClassForExecution;
-import org.reflections.Reflections;
-
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-
 import java.util.Arrays;
 import java.util.Set;
 
-public class StarterClass {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.reflections.Reflections;
 
-    public static void main(String[] args) {
-        Reflections rfl = new Reflections("com.jan");
-        Set<Class<?>> annotatedClasses = rfl.getTypesAnnotatedWith(PickClassForExecution.class);
-        if(!annotatedClasses.isEmpty()){
-            for(Class cls: annotatedClasses){
-                Method[] methods = cls.getMethods();
-                for(Method mth : methods){
-                    Annotation[] annotations = mth.getAnnotations();
-                    if(Arrays.stream(annotations).anyMatch(item->item.annotationType().getName().equalsIgnoreCase("com.jan.commons.annotations.PickMethodForExecution"))){
-                        try {
-                            mth.invoke(Arrays.stream(cls.getDeclaredConstructors()).findFirst().get().newInstance());
-                        } catch (IllegalAccessException e) {
-                            throw new RuntimeException(e);
-                        } catch (InvocationTargetException e) {
-                            throw new RuntimeException(e);
-                        } catch (InstantiationException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-                }
-            }
-        }
-        System.out.println(annotatedClasses.toString());
-        System.out.println("Hello");
-}
+import com.jan.commons.annotations.PickClassForExecution;
+
+public class StarterClass {
+	private static final Logger log = LogManager.getLogger(StarterClass.class); 
+
+	public static void main(String[] args) {
+		Reflections rfl = new Reflections("com.jan");
+		Set<Class<?>> annotatedClasses = rfl.getTypesAnnotatedWith(PickClassForExecution.class);
+		if (!annotatedClasses.isEmpty()) {
+			for (Class cls : annotatedClasses) {
+				Method[] methods = cls.getMethods();
+				for (Method mth : methods) {
+					Annotation[] annotations = mth.getAnnotations();
+					if (Arrays.stream(annotations).anyMatch(item -> item.annotationType().getName()
+							.equalsIgnoreCase("com.jan.commons.annotations.PickMethodForExecution"))) {
+						try {
+							Constructor cns = getZeroArgConstructorOfClass(cls);
+							if (cns != null) {
+								mth.invoke(cns.newInstance());
+							}
+						} catch (IllegalAccessException e) {
+							throw new RuntimeException(e);
+						} catch (InvocationTargetException e) {
+							throw new RuntimeException(e);
+						} catch (InstantiationException e) {
+							throw new RuntimeException(e);
+						}
+					}
+				}
+			}
+		}
+		log.info("Completed");
+	}
+
+	private static Constructor getZeroArgConstructorOfClass(Class cls) {
+		Constructor[] declaredConstructors = cls.getDeclaredConstructors();
+		Constructor cns = null;
+		for (int place = 0; place < declaredConstructors.length; place++) {
+			if (declaredConstructors[place].getParameterCount() == 0) {
+				cns = declaredConstructors[place];
+				return cns;
+			}
+		}
+		return null;
+	}
 }
---
+++ src/main/java/com/jan/romanToInteger/v1/RomanToIntegerImpl.java
@@ -0,0 +1,97 @@
+package com.jan.romanToInteger.v1;
+
+import java.util.AbstractMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.jan.commons.annotations.PickClassForExecution;
+import com.jan.commons.annotations.PickMethodForExecution;
+
+@PickClassForExecution
+public class RomanToIntegerImpl {
+	private static final Logger log = LogManager.getLogger(RomanToIntegerImpl.class);
+
+	private String input;
+
+	private static Map<String, String> allowedRomanMap = Stream
+			.of(new AbstractMap.SimpleImmutableEntry<String, String>("V", "I"),
+					new AbstractMap.SimpleImmutableEntry<String, String>("X", "I"),
+					new AbstractMap.SimpleImmutableEntry<String, String>("L", "X"),
+					new AbstractMap.SimpleImmutableEntry<String, String>("C", "X"),
+					new AbstractMap.SimpleImmutableEntry<String, String>("D", "C"),
+					new AbstractMap.SimpleImmutableEntry<String, String>("M", "C"))
+			.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+	private static Map<String, Integer> romanValueMap = Stream
+			.of(new AbstractMap.SimpleImmutableEntry<String, Integer>("I", Integer.valueOf(1)),
+					new AbstractMap.SimpleImmutableEntry<String, Integer>("V", Integer.valueOf(5)),
+					new AbstractMap.SimpleImmutableEntry<String, Integer>("X", Integer.valueOf(10)),
+					new AbstractMap.SimpleImmutableEntry<String, Integer>("L", Integer.valueOf(50)),
+					new AbstractMap.SimpleImmutableEntry<String, Integer>("C", Integer.valueOf(100)),
+					new AbstractMap.SimpleImmutableEntry<String, Integer>("D", Integer.valueOf(500)),
+					new AbstractMap.SimpleImmutableEntry<String, Integer>("M", Integer.valueOf(1000)))
+			.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+	public RomanToIntegerImpl() {
+	}
+
+	public RomanToIntegerImpl(String strInput) {
+		strInput = strInput.toUpperCase();
+		boolean proceed = proceedIfValidInput(strInput);
+		if (proceed) {
+			input = strInput;
+		} else {
+			input = "";
+		}
+	}
+
+	private boolean proceedIfValidInput(String strInput) {
+		String regex = "[IVXLCDM]";
+		Pattern ptn = Pattern.compile(regex);
+		Matcher matcher = ptn.matcher(strInput);
+		return matcher.find();
+	}
+
+	public String getInput() {
+		return input;
+	}
+
+	public void setInput(String input) {
+		this.input = input;
+	}
+
+	@PickMethodForExecution
+	public int solve() {
+		int response = 0;
+		String sample = "MCMXCCCLIV";
+		RomanToIntegerImpl impl = new RomanToIntegerImpl(sample);
+		response = impl.getIntegerValue();
+		return response;
+	}
+
+	private int getIntegerValue() {
+		int output = 0;
+		int currentVal = 0;
+		for (int place = 0; place < input.length(); place++) {
+			String currentSymbol = String.valueOf(input.charAt(place));
+			currentVal = romanValueMap.get(currentSymbol);
+			if (place + 1 < input.length()) {
+				String nextSymbol = String.valueOf(input.charAt(place + 1));
+				if (nextSymbol != null && allowedRomanMap.get(nextSymbol) != null && allowedRomanMap.get(nextSymbol).equalsIgnoreCase(currentSymbol)) {
+					currentVal = romanValueMap.get(nextSymbol) - currentVal;
+					place++;
+				}
+			}
+			output = output + currentVal;
+		}
+		log.info("Input: {}; output: {} " , input , output);
+		return output;
+	}
+
+}
